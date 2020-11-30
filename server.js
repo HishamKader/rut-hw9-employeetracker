a6eb45fc398222c426666e481e1de7cf3071084e
Hisham Kader <hisham319@hotmail.com>
2020-11-30 16:10:09 -0500

    added new lines of code that lest you view data in command line

--- server.js
+++ server.js
@@ -1,69 +1,118 @@
-const mysql = require("mysql");
 const inquirer = require("inquirer");
+const mysql = require("mysql");
 const cTable = require("console.table");
 
-
-var connection = mysql.createConnection({
+// Create the mysql connection, store in variable.
+let connection = mysql.createConnection({
     host: "localhost",
-
-    port: 3306,
-
-    user: "root",
-
-    password: "Password",
     
+    port: 3306,
+    
+    user: "root",
+   
+    password: "password",
+   
     database: "employee_db"
 });
 
-connection.connect(function (err) {
+// Connect to the database.
+connection.connect((err) => {
     if (err) throw err;
-
-    start();
+    getUserInput();
 });
-
-console.log("Welcome to our Employee Tracker Database");
-
+console.log("Welcome to the Employee Tracker DataBase!\n Use the selector below to get started.")
 /*
-  getUserInput() uses inquirer to determine if the user wants to simply
-  view the database, add to the database, or delete from the database.
+    getUserInput() uses inquirer to determine if the user wants to simply
+    view the database, add to the database, or delete from the database.
 */
-
 function getUserInput() {
     inquirer.prompt(
-
         [
             {
                 name: "startOptions",
                 type: "list",
-                message: "Make a selection",
-                choice: [
-                    "add data",
+                message: "Make a selection: ",
+                choices: [
+                    "Add Data",
                     "View Data",
                     "Update Data",
                     "Delete Data",
                     "Exit"
-
                 ]
             }
-
         ]).then((answer) => {
             if (answer.startOptions === "View Data") {
-                ViewData();
+                viewData();
             }
             else if (answer.startOptions === "Add Data") {
                 addData();
             }
             else if (answer.startOptions === "Update Data") {
-                UpdateData();
+                updateData();
             }
             else if (answer.startOptions === "Delete Data") {
-                DeleteData();
+                deleteData();
             }
-            else if (answer.startOptions === "Delete Data") {
-                process.exit(1)
+            else if (answer.startOptions === "Exit") {
+                process.exit(1);
             }
         })
-
 }
+/*
+    addData() asks the user what information they want to add to
+    the database tables, including employees, roles and departments.
+*/
+function addData() {
+    inquirer.prompt([
+        {
+            name: "addOptions",
+            type: "list",
+            message: "Select what you want to add to the database: ",
+            choices: [
+                "Add Employee",
+                "Add Role",
+                "Add Department",
+                "Exit"
+            ]
+        }
+    ]).then(function (answer) {
+        if (answer.addOption === "Add Employee") {
+            inquirer.prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "First Name: "
+                },
 
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "Last Name: "
+                },
+                {
+                    name: "role_id",
+                    type: "number",
+                    message: "Role ID #: "
+                }
+            ]).then(function (answer) {
+                connection.query(
+                    "INSERT INTO employee SET ?",
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        role_id: answer.role_id
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        console.log(`Added employee, ${answer.first_name} ${answer.last_name}`);
+                        getUserInput();
+                    }
+                )
+            })
+        } else if (answer.addOption === "Add Role") {
+            inquirer.prompt([
 
+            ])
+        }
+    });
+}
